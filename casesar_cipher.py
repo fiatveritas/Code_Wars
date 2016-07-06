5eccfed3d19142f08d4df49c5be1cfc782cd9acf
Jesse Gallegos <gllgsjss@gmail.com>
2016-07-05 20:23:41 -0700

    changed conditional statements, mod 25

--- casesar_cipher.py
+++ casesar_cipher.py
@@ -6,29 +6,36 @@ def encryptor(key, message):
     else: 
         n = 0
         new_string = ''
+        lister = []
+        upper = [i for i in string.ascii_uppercase]
+        lower = [i for i in string.ascii_lowercase]
         letter_pos = None
         while n < len(message) :
-            if message[n].isupper:
-            	letter_pos = string.ascii_uppercase.index(message[n])
+            if message[n].isupper():
+            	letter_pos = upper.index(message[n])
             	letter_pos += key
-            	new_string.append(string.ascii_uppercase[letter_pos])
-            elif message[n].islower:
-            	letter_pos = string.ascii_lowercase.index(message[n])
+            	while letter_pos < 0 or letter_pos > 25:
+            		letter_pos %= 26
+            	lister.append(upper[letter_pos])
+            elif message[n].islower():
+            	letter_pos = lower.index(message[n])
             	letter_pos += key
-            	new_string.append(string.ascii_lowercase[letter_pos])
-            	print(message[n])
+            	while letter_pos < 0 or letter_pos > 25:
+            		letter_pos %= 26
+            	lister.append(lower[letter_pos])
+            	print message[n]
             else:
-            	new_string.append(message[n])
-            	print(message[n])
+            	lister.append(message[n])
+            	print message[n]
             n += 1
-        return new_string
+        return new_string.join(lister)
     #print(message)
     #print(new_string)
     #return new_string
 
 #print(encryptor(13, ''))
-print(encryptor(13, 'Caesar Cipher')) #'Pnrfne Pvcure'
+#print encryptor(13, 'Caesar Cipher') #'Pnrfne Pvcure'
 #print(encryptor(-5, 'Hello World!')) #'Czggj Rjmgy!'
-#print(encryptor(27, 'Whoopi Goldberg') #'Xippqj Hpmecfsh'
+#print encryptor(27, 'Whoopi Goldberg') #'Xippqj Hpmecfsh'
 #print(encryptor(39, "T")) #'G
 #print('ABV'.isupper())
