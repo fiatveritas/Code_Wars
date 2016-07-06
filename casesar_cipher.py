4048212e36baac8bdc91eb8bc87d6bfa7af3d0f9
Jesse Gallegos <gllgsjss@gmail.com>
2016-07-05 18:57:13 -0700

    undid a few things

--- casesar_cipher.py
+++ casesar_cipher.py
@@ -6,14 +6,20 @@ def encryptor(key, message):
     else: 
         n = 0
         new_string = ''
-        pos_letter = None
-        while( n < len(message)):
-            letter_pos = ord(message[n])
-            if letter_pos >= 65 and letter_pos <= 90:
+        letter_pos = None
+        while n < len(message) :
+            if message[n].isupper:
+            	letter_pos = string.ascii_uppercase.index(message[n])
             	letter_pos += key
-            	while(not(letter_pos >= 65 and letter_pos <= 90)):
-            		letter_pos %= 26
-                print(unichr(letter_pos))
+            	new_string.append(string.ascii_uppercase[letter_pos])
+            elif message[n].islower:
+            	letter_pos = string.ascii_lowercase.index(message[n])
+            	letter_pos += key
+            	new_string.append(string.ascii_lowercase[letter_pos])
+            	print(message[n])
+            else:
+            	new_string.append(message[n])
+            	print(message[n])
             n += 1
         return new_string
     #print(message)
@@ -21,8 +27,8 @@ def encryptor(key, message):
     #return new_string
 
 #print(encryptor(13, ''))
-#print(encryptor(13, 'Caesar Cipher')) #'Pnrfne Pvcure'
-print(encryptor(-5, 'Hello World!')) #'Czggj Rjmgy!'
+print(encryptor(13, 'Caesar Cipher')) #'Pnrfne Pvcure'
+#print(encryptor(-5, 'Hello World!')) #'Czggj Rjmgy!'
 #print(encryptor(27, 'Whoopi Goldberg') #'Xippqj Hpmecfsh'
 #print(encryptor(39, "T")) #'G
 #print('ABV'.isupper())
