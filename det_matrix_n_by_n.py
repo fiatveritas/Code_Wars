b2b3f3baa01ae615d5d2405436b851ae89380aa6
Jesse Gallegos <gllgsjss@gmail.com>
2016-07-06 22:50:45 -0700

    For det (#3)
    * created file, included test cases
    
    * added bases, 1-d & 2-d
    
    * started else statement
    
    * added more lines and defined minor expansion
    
    * corrected else conditional, had error with index function, kept giving first occurence

---
+++ det_matrix_n_by_n.py
@@ -0,0 +1,22 @@
+def determinant(matrix):
+    if len(matrix) == 1:
+        return matrix[0][0]
+    elif len(matrix) == 2:
+        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+        return det
+    else:
+        sum = 0
+        for i in range(len(matrix[0])):
+            sum += (-1) ** i * matrix[0][i] * determinant(minor_exp(i, matrix))
+        return sum
+def minor_exp(column, matrix):
+    i = 1
+    new_matrix = []
+    while i < len(matrix):
+        temp_matrix = []
+        for j in range(len(matrix)):
+          if j != column:
+            temp_matrix.append(matrix[i][j])
+        new_matrix.append(temp_matrix)
+        i += 1
+    return new_matrix
