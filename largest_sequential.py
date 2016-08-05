ab8e96ecaf39f2e61c6cebd4f9c56fa2c2aabd63
fiatveritas <jesse.gallegos@usc.edu>
2016-08-04 21:48:37 -0700

    fix: fixing code for test case got code to work with most test cases reminder keep using continue try making list of numbers and take min of that list

--- largest_sequential.py
+++ largest_sequential.py
@@ -1,10 +1,32 @@
-!#/usr/bin/python2.7
+#!/usr/bin/python2.7
 def next_bigger(n):
-    for i in str(n):
-    	print int(i)
+    print n
+    number = [int(i) for i in str(n)]
+    copy = number[:]
+    index = len(copy) - 1
+    list_num = []
+    while index >= 0:
+        for i in range(len(copy) - 1, -1, -1):
+            if index == i:
+                continue
+            elif copy[index] == copy[i]:
+                continue
+            elif copy[index] > copy[i]:
+                digit_holder = None
+                digit_holder = copy[index]
+                copy[index] = copy[i]
+                copy[i] = digit_holder
+                stringer = ''
+                for digit in copy:
+                    stringer += str(digit)
+                list_num.append(int(stringer))
+                index = 0
+                continue
+        index -= 1
+    return min(list_num)
 
-next_bigger(12) #21
-#next_bigger(513) #531
-#next_bigger(2017) #2071
-#next_bigger(414) #441
-#next_bigger(144) #414
+print next_bigger(12) #21
+print next_bigger(513) #531
+print next_bigger(2017) #2071
+print next_bigger(414) #441
+print next_bigger(144) #414
