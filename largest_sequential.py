5a9378d3329f80c988fc04e9b77a938e03b1b639
Jesse Gallegos <gllgsjss@gmail.com>
2016-08-09 23:53:08 -0700

    feat: recoded a bug for repeated numbers

--- largest_sequential.py
+++ largest_sequential.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python2.7
+import random
 def arranged(n, copy):
     stringer = ''
     index = len(copy) - 1
-    while index >= 0:
+    while index > -1:
         if copy[index - 1] < copy[index]:
             place_holder = index - 1
             antecedent_copy = copy[:place_holder + 1]
             after_copy = copy[place_holder + 1:]
+            after_copy.sort()
             modified_copy = after_copy[:]
             print antecedent_copy, after_copy
-            modified_copy = after_copy[:]
-            after_copy.sort()
-            for i in after_copy:
-                if antecedent_copy[place_holder] < i:
+            j = 0
+            while j < len(after_copy):
+                if antecedent_copy[place_holder] < after_copy[j]:
                     holder = None
                     holder = antecedent_copy[place_holder]
-                    antecedent_copy[place_holder] = i
-                    modified_copy[modified_copy.index(i)] = holder
+                    antecedent_copy[place_holder] = modified_copy[j]
+                    modified_copy[j] = holder
                     break
+                j += 1
             modified_copy.sort()
-            for i in antecedent_copy:
-                stringer += str(i)
-            for i in modified_copy:
-                stringer += str(i)
-            print antecedent_copy, modified_copy
             #print place_holder
             break
         index -= 1
-    return int(stringer)
-
+    for i in antecedent_copy:
+        stringer += str(i)
+    for i in modified_copy:
+        stringer += str(i)
+    stringer = int(stringer)
+    print antecedent_copy, modified_copy
+    return stringer
 def next_bigger(n):
     print '*************', n
+    if type(n) is not int:
+        return -1
     number = [int(i) for i in str(n)]
     copy = number[:]
     index = len(copy) - 1
@@ -40,14 +44,15 @@ def next_bigger(n):
             return -1
         index -= 1
     index = len(copy) - 1
-    while number[0] == number[index]:
+    while index > -1 and number[0] == number[index]:
         if index == 0:
             return -1
         index -= 1
-    arranged(n, copy)
+    return arranged(n, copy)
 
-
-
+#for i in [random.randint(0, 20000) for j in range(0,200)]:
+#    print i
+#    print next_bigger(i)
 
 print next_bigger(12) #21
 print next_bigger(513) #531
@@ -57,3 +62,6 @@ print next_bigger(144) #414
 print next_bigger(1234567980)
 #print next_bigger(98765421)
 #print next_bigger(1111)
+#print next_bigger(3)
+print next_bigger(7555)
+#print next_bigger('')
